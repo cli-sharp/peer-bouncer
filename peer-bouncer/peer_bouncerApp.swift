94d34aedf2d08c293b9997d24c3db7f8a8717363
Christian Linse <christian.linse@gmail.com>
2025-09-19 18:27:03 +0200

    Template

---
+++ peer-bouncer/peer_bouncerApp.swift
@@ -0,0 +1,17 @@
+//
+//  peer_bouncerApp.swift
+//  peer-bouncer
+//
+//  Created by Christian Linse on 19.09.25.
+//
+
+import SwiftUI
+
+@main
+struct peer_bouncerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
